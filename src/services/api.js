0d9f69297eb0f06b5f9018bc19a17a0adeafa98d
Piod Gamer <malickelhadji07@gmail.com>
2025-05-29 14:13:29 +0000

    deployement

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    "https://projetstagebackend-2.onrender.com/api",
 });
 
 // Initialiser le token depuis localStorage si présent
